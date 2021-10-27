ec7242df35762d80e162545aeccf72f5219a7e50
juliuschristopher <jcb218@cam.ac.uk>
2021-10-27 15:57:13 +0100

    Supervised_T_cell_analysis

---
+++ Supervised_T_cells_analysis.R
@@ -0,0 +1,16 @@
+## ##Analysis of ANALYSED flow data - T cells####
+setwd("~/Google Drive/Shared drives/Okkengroup/Experiments/Julius/Experiments/Phenotyping_Cyp11a1_E1020K_Summary/Supervised_Cyp11a1_Phenotyping")
+
+####Load all required packages####
+library(tidyverse)
+library(ggfortify)
+library(readxl)
+library(geaom)
+library(ggbeeswarm)
+library(RColorBrewer)
+library(viridis)
+library(ggthemes)
+library(ggforce)
+library(car)
+library(umap)
+
